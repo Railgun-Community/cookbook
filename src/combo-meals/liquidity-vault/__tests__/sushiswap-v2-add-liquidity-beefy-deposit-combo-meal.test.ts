2fa9688eb4b8d52ad7e937aebdf0c704d98c28f8
jrmeurer <jrmeurer@wisc.edu>
2023-05-15 11:17:01 -0600

    Use mock to fix liq test

--- src/combo-meals/liquidity-vault/__tests__/sushiswap-v2-add-liquidity-beefy-deposit-combo-meal.test.ts
+++ src/combo-meals/liquidity-vault/__tests__/sushiswap-v2-add-liquidity-beefy-deposit-combo-meal.test.ts
@@ -22,6 +22,8 @@ import {
   getUnshieldFee,
   getUnshieldedAmountAfterFee,
 } from '../../../utils/fee';
+import { UniV2LikeFactoryContract } from '../../../contract/liquidity/uni-v2-like-factory-contract';
+import { ZERO_ADDRESS } from '../../../models/constants';
 
 chai.use(chaiAsPromised);
 const { expect } = chai;
@@ -65,6 +67,7 @@ let dateStub: SinonStub;
 let uniswapV2PairGetReserves: SinonStub;
 let uniswapV2PairTotalSupply: SinonStub;
 let beefyVaultForIDStub: SinonStub;
+let uniswapV2FactoryFeeToStub: SinonStub;
 
 const provider = new JsonRpcProvider(
   'https://eth-mainnet.g.alchemy.com/v2/demo',
@@ -93,6 +96,10 @@ describe('sushiswap-v2-add-liquidity-beefy-deposit-combo-meal', () => {
       UniV2LikePairContract.prototype,
       'totalSupply',
     ).resolves(oneInDecimals18.mul(2_000_000));
+    uniswapV2FactoryFeeToStub = Sinon.stub(
+      UniV2LikeFactoryContract.prototype,
+      'feeTo',
+    ).resolves(ZERO_ADDRESS);
   });
 
   after(() => {
@@ -100,6 +107,7 @@ describe('sushiswap-v2-add-liquidity-beefy-deposit-combo-meal', () => {
     uniswapV2PairGetReserves.restore();
     uniswapV2PairTotalSupply.restore();
     beefyVaultForIDStub.restore();
+    uniswapV2FactoryFeeToStub.restore();
   });
 
   it('Should create sushiswap-v2-add-liquidity-beefy-deposit-combo-meal', async () => {
@@ -281,8 +289,8 @@ describe('sushiswap-v2-add-liquidity-beefy-deposit-combo-meal', () => {
       feeERC20AmountRecipients: [],
       outputERC20Amounts: [
         {
-          expectedBalance: BigNumber.from('0x1bb3d96af7775df9'),
-          minBalance: BigNumber.from('0x1b6cee489df39d09'),
+          expectedBalance: BigNumber.from('0x1bafa9ee16e78000'),
+          minBalance: BigNumber.from('0x1b68c982bfa2a000'),
           approvedSpender: undefined,
           isBaseToken: false,
           tokenAddress: LP_TOKEN.tokenAddress,
@@ -292,7 +300,7 @@ describe('sushiswap-v2-add-liquidity-beefy-deposit-combo-meal', () => {
       outputNFTs: [],
       populatedTransactions: [
         {
-          data: '0xe8e33700000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb480000000000000000000000000000000000000000000000000dd7d4f70b73bfff0000000000000000000000000000000000000000000000000000000076e948c00000000000000000000000000000000000000000000000000db464c15fd150000000000000000000000000000000000000000000000000000000000075b8df100000000000000000000000004025ee6512dbbda97049bcf5aa5d38c54af6be8a0000000000000000000000000000000000000000000000000000000005fa74e0',
+          data: '0xe8e33700000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2000000000000000000000000a0b86991c6218b36c1d19d4a2e9eb0ce3606eb480000000000000000000000000000000000000000000000000dd7d4f70b73c0000000000000000000000000000000000000000000000000000000000076e948c00000000000000000000000000000000000000000000000000db464c15fd150000000000000000000000000000000000000000000000000000000000075b8df100000000000000000000000004025ee6512dbbda97049bcf5aa5d38c54af6be8a0000000000000000000000000000000000000000000000000000000005fa74e0',
           to: '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F',
         },
       ],
@@ -320,8 +328,8 @@ describe('sushiswap-v2-add-liquidity-beefy-deposit-combo-meal', () => {
       outputERC20Amounts: [
         {
           approvedSpender: vault.vaultContractAddress,
-          expectedBalance: BigNumber.from('0x1bb3d96af7775df9'),
-          minBalance: BigNumber.from('0x1bb3d96af7775df9'), // TODO: 0x1b6cee489df39d09 minimum is lost from recipe output into combo.
+          expectedBalance: BigNumber.from('0x1bafa9ee16e78000'),
+          minBalance: BigNumber.from('0x1bafa9ee16e78000'), // TODO: Fix this: Actual minimum is lost from recipe output into combo.
           isBaseToken: false,
           tokenAddress: LP_TOKEN.tokenAddress,
           decimals: 18,
@@ -352,8 +360,8 @@ describe('sushiswap-v2-add-liquidity-beefy-deposit-combo-meal', () => {
       outputERC20Amounts: [
         {
           approvedSpender: undefined,
-          expectedBalance: BigNumber.from('0x1b674aafe6aabe86'),
-          minBalance: BigNumber.from('0x1b674aafe6aabe86'),
+          expectedBalance: BigNumber.from('0x1b6326c42440912b'),
+          minBalance: BigNumber.from('0x1b6326c42440912b'),
           tokenAddress: vault.vaultTokenAddress,
           decimals: 18,
         },
@@ -367,7 +375,7 @@ describe('sushiswap-v2-add-liquidity-beefy-deposit-combo-meal', () => {
       ],
       spentERC20Amounts: [
         {
-          amount: BigNumber.from('0x1bb3d96af7775df9'),
+          amount: BigNumber.from('0x1bafa9ee16e78000'),
           tokenAddress: LP_TOKEN.tokenAddress,
           recipient: 'ETH-USDC LP Vault',
           decimals: 18,
@@ -381,7 +389,7 @@ describe('sushiswap-v2-add-liquidity-beefy-deposit-combo-meal', () => {
       description: 'Shield ERC20s and NFTs into private RAILGUN balance.',
       feeERC20AmountRecipients: [
         {
-          amount: BigNumber.from('0x1189c966562fd6'),
+          amount: BigNumber.from('0x1187230ce4005c'),
           recipient: 'RAILGUN Shield Fee',
           tokenAddress: vault.vaultTokenAddress,
           decimals: 18,
@@ -390,8 +398,8 @@ describe('sushiswap-v2-add-liquidity-beefy-deposit-combo-meal', () => {
       outputERC20Amounts: [
         {
           approvedSpender: undefined,
-          expectedBalance: BigNumber.from('0x1b55c0e680548eb0'),
-          minBalance: BigNumber.from('0x1b55c0e680548eb0'),
+          expectedBalance: BigNumber.from('0x1b519fa1175c90cf'),
+          minBalance: BigNumber.from('0x1b519fa1175c90cf'),
           tokenAddress: vault.vaultTokenAddress,
           isBaseToken: undefined,
           decimals: 18,
@@ -443,7 +451,7 @@ describe('sushiswap-v2-add-liquidity-beefy-deposit-combo-meal', () => {
         decimals: LP_TOKEN.decimals,
       },
       {
-        amount: BigNumber.from('0x1189c966562fd6'),
+        amount: BigNumber.from('0x1187230ce4005c'),
         recipient: 'RAILGUN Shield Fee',
         tokenAddress: vault.vaultTokenAddress,
         decimals: vault.depositERC20Decimals,
