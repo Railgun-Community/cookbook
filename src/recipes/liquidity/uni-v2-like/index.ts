e2ab0670a93048cdd6c4df1c4e82d20a7885dbac
Pete S <ptaylorsimpson@gmail.com>
2023-08-15 15:08:56 -0700

    Export all liquidity recipe classes

--- src/recipes/liquidity/uni-v2-like/index.ts
+++ src/recipes/liquidity/uni-v2-like/index.ts
@@ -2,3 +2,5 @@ export * from './sushiswap-v2-add-liquidity-recipe';
 export * from './sushiswap-v2-remove-liquidity-recipe';
 export * from './uniswap-v2-add-liquidity-recipe';
 export * from './uniswap-v2-remove-liquidity-recipe';
+export * from './uni-v2-like-add-liquidity-recipe';
+export * from './uni-v2-like-remove-liquidity-recipe';
