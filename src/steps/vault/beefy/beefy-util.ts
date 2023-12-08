bba5a8e2006a4ad4d94d65e530ea8da3e070ea2b
Pete S <ptaylorsimpson@gmail.com>
2023-12-08 14:46:56 -0800

    Updates wallet sdk. Fixes Beefy Vaults recipe issues with 6 decimal deposit tokens

--- src/steps/vault/beefy/beefy-util.ts
+++ src/steps/vault/beefy/beefy-util.ts
@@ -4,21 +4,17 @@ export const calculateOutputsForBeefyDeposit = (
   stepInitialBalance: bigint,
   vault: BeefyVaultData,
 ) => {
-  const { depositERC20Decimals, vaultRate, depositFeeBasisPoints } = vault;
+  const { vaultRate, depositFeeBasisPoints } = vault;
 
   const depositFeeAmount =
     (stepInitialBalance * depositFeeBasisPoints) / 10000n;
   const depositAmountAfterFee = stepInitialBalance - depositFeeAmount;
 
-  const depositERC20DecimalsAdjustment = 10n ** BigInt(depositERC20Decimals);
   const vaultERC20DecimalsAdjustment =
     10n ** BigInt(BEEFY_VAULT_ERC20_DECIMALS);
-  const decimalQuotient =
-    vaultERC20DecimalsAdjustment / depositERC20DecimalsAdjustment;
 
   const receivedVaultTokenAmount =
-    (depositAmountAfterFee * vaultERC20DecimalsAdjustment * decimalQuotient) /
-    vaultRate;
+    (depositAmountAfterFee * vaultERC20DecimalsAdjustment) / vaultRate;
 
   return {
     depositFeeAmount,
@@ -31,18 +27,13 @@ export const calculateOutputsForBeefyWithdraw = (
   stepInitialBalance: bigint,
   vault: BeefyVaultData,
 ) => {
-  const { depositERC20Decimals, vaultRate, withdrawFeeBasisPoints } = vault;
+  const { vaultRate, withdrawFeeBasisPoints } = vault;
 
-  const depositERC20DecimalsAdjustment = 10n ** BigInt(depositERC20Decimals);
   const vaultERC20DecimalsAdjustment =
     10n ** BigInt(BEEFY_VAULT_ERC20_DECIMALS);
-  const decimalQuotient =
-    vaultERC20DecimalsAdjustment / depositERC20DecimalsAdjustment;
 
   const receivedWithdrawAmount =
-    (stepInitialBalance * vaultRate) /
-    decimalQuotient /
-    vaultERC20DecimalsAdjustment;
+    (stepInitialBalance * vaultRate) / vaultERC20DecimalsAdjustment;
 
   const withdrawFeeAmount =
     (receivedWithdrawAmount * withdrawFeeBasisPoints) / 10000n;
