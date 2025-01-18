cdd89dfa3ffd6489cfa72feb4f891e5111e3eb22
zy0n <zy0n.barto@gmail.com>
2025-01-18 10:39:52 -0500

    feat: add Lido STETH and WSTETH ABI imports to the ABI module

--- src/abi/abi.ts
+++ src/abi/abi.ts
@@ -5,6 +5,8 @@ import ABI_UNI_V2_LIKE_FACTORY from './liquidity/UniV2LikeFactory.json';
 import ABI_UNI_V2_LIKE_ROUTER from './liquidity/UniV2LikeRouter.json';
 import ABI_UNI_V2_LIKE_PAIR from './liquidity/UniV2LikePair.json';
 import ABI_BEEFY_VAULT_MERGED_V6V7 from './vault/beefy/BeefyVault-MergedV6V7.json';
+import ABI_LIDO_STETH from './lido/LidoSTETH.json';
+import ABI_LIDO_WSTETH from './lido/LidoWSTETH.json';
 
 export const abi = {
   token: {
@@ -13,6 +15,10 @@ export const abi = {
   },
   adapt: {
     relay: ABI_RELAY_ADAPT,
+  },
+  lido: {
+    steth: ABI_LIDO_STETH,
+    wsteth: ABI_LIDO_WSTETH,
   },
   liquidity: {
     uniV2LikeFactory: ABI_UNI_V2_LIKE_FACTORY,
